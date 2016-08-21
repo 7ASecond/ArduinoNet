0c2c832792d297dbaffa6c694ca21ca3425bc948
Dave Gordon <admin@7asecond.net>
2016-08-21 04:03:18 +0100

    CSV Data Saving completed

--- ArduinoNet-TempCSV/Form1.Designer.cs
+++ ArduinoNet-TempCSV/Form1.Designer.cs
@@ -28,12 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tracker = new WinControls.Tracker();
+            this.rtbConOut = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // tracker
+            // 
+            this.tracker.BackColor = System.Drawing.Color.Navy;
+            this.tracker.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tracker.ForeColor = System.Drawing.Color.Transparent;
+            this.tracker.Location = new System.Drawing.Point(0, 0);
+            this.tracker.Name = "tracker";
+            this.tracker.Size = new System.Drawing.Size(897, 191);
+            this.tracker.TabIndex = 0;
+            this.tracker.Text = "tracker1";
+            // 
+            // rtbConOut
+            // 
+            this.rtbConOut.BackColor = System.Drawing.Color.MidnightBlue;
+            this.rtbConOut.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rtbConOut.ForeColor = System.Drawing.Color.Yellow;
+            this.rtbConOut.Location = new System.Drawing.Point(0, 191);
+            this.rtbConOut.Name = "rtbConOut";
+            this.rtbConOut.Size = new System.Drawing.Size(897, 108);
+            this.rtbConOut.TabIndex = 1;
+            this.rtbConOut.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(897, 335);
+            this.Controls.Add(this.rtbConOut);
+            this.Controls.Add(this.tracker);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Save Arduino Data as CSV";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private WinControls.Tracker tracker;
+        private System.Windows.Forms.RichTextBox rtbConOut;
     }
 }
 
--- ArduinoNet-TempCSV/Form1.cs
+++ ArduinoNet-TempCSV/Form1.cs
@@ -3,18 +3,98 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ArduinoNet;
 
 namespace ArduinoNet_TempCSV
 {
     public partial class Form1 : Form
     {
+        public delegate void ControlStringConsumer(RichTextBox control, string text);  // defines a delegate type
+        public delegate void ControlDecimalConsumer(Decimal value);  // defines a delegate type
+        private readonly Serial _serial = new Serial("COM9", 115200, false);
+
         public Form1()
         {
             InitializeComponent();
+            _serial.ArduinoDataRecievedEvent += _serial_ArduinoDataRecievedEvent;
+            _serial.OpenConnection();
+        }
+
+        private string _lastValue = "0";
+        private void _serial_ArduinoDataRecievedEvent(string dataRecievedMessage)
+        {
+            SetText(rtbConOut,dataRecievedMessage);
+            try
+            {
+                if (dataRecievedMessage.Contains('\r')) dataRecievedMessage = dataRecievedMessage.Replace("\r", "");
+              
+                    SetDecimal(Convert.ToDecimal(dataRecievedMessage));              
+            }
+            catch (Exception)
+            {
+                SetDecimal(Convert.ToDecimal(_lastValue));
+            }
+
+            _lastValue = dataRecievedMessage;
+        }
+
+        /// <summary>
+        /// This updates the Tracker Graph 
+        /// </summary>
+        /// <param name="toDecimal">
+        /// Decimal: The new value for the tracker
+        /// </param>
+        private void SetDecimal(decimal toDecimal)
+        {
+            if (tracker.InvokeRequired)
+            {
+                tracker.Invoke(new ControlDecimalConsumer(SetDecimal), new object[] { toDecimal });  // invoking itself
+            }
+            else
+            {
+                if ((int)(toDecimal * 100) + 100 > tracker.Maximum)
+                    tracker.Maximum = (int)(toDecimal * 100) + 100;
+                tracker.Value = (int)(toDecimal * 100);      // the "functional part", executing only on the main thread
+            }
+        }
+
+        /// <summary>
+        /// This updates the Log RichTextBox
+        /// </summary>
+        /// <param name="control">
+        /// RichTextBox: The control to update
+        /// </param>
+        /// <param name="text">
+        /// string: The information to add to the log control
+        /// </param>
+        public void SetText(RichTextBox control, string text)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new ControlStringConsumer(SetText), new object[] { control, text });  // invoking itself
+            }
+            else
+            {
+                control.AppendText(DateTime.Now + "\t" + text);      // the "functional part", executing only on the main thread
+                SaveData(DateTime.Now + "," + text); // use a comma instead of a tab \t to make a CSV file
+            }
+        }
+
+        // File saved to desktop
+        private readonly StreamWriter _sw = File.AppendText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "ArduinoData.txt"));
+        private void SaveData(string data)
+        {
+            _sw.WriteLineAsync(data + Environment.NewLine); // may write out of turn but the TimeStamp will allow us to sort it and put it back together correctly
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _sw.Close();
         }
     }
 }
